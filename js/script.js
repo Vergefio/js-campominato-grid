f930e48c7eedb3e53810ff3589bf93469d40914e
Vergefio <riccardods@live.it>
2022-10-20 18:35:11 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -4,22 +4,24 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
-const container = document.quareySelector('.container');
+const container = document.querySelector('.container');
 const numElements = 100
 const numberSquare = []
 
-for (let i = 1; i < 101; i++) {
-
+function createSquare(val) {
   const square = document.createElement('div');
   square.className = 'square';
-  console.log(square);
-  square.innerHTML = i;
-
+  square.innerText = val;
+  square.idElement = val;
   square.addEventListener('click', function () {
-    console.log('cliccato');
-    this.classList.add('azzurro');
-  })
-
+    console.log(this.idElement)
+  });
 
   container.append(square);
+}
+
+console.log(numElements);
+
+for (let i = 0; i < numElements; i++) {
+  createSquare(i + 1);
 }
